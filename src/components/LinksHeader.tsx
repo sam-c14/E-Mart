dc3eedf3a6f5d1229a56c239a906c23874f22fbf
sam-c14 <samuelaffah14@gmail.com>
2023-04-03 19:21:58 +0100

    Links Header Component for Home Page added

---
+++ src/components/LinksHeader.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { SiYourtraveldottv } from "react-icons/si";
+import { MdNoFood } from "react-icons/md";
+import { ImPaypal } from "react-icons/im";
+import { MdLocalDrink } from "react-icons/md";
+import { GiHealthNormal } from "react-icons/gi";
+import { BiBusSchool } from "react-icons/bi";
+import { MdLocalGroceryStore } from "react-icons/md";
+
+const LinksHeader = () => {
+  const icons = [
+    <SiYourtraveldottv />,
+    <MdNoFood />,
+    <ImPaypal />,
+    <MdLocalDrink />,
+    <GiHealthNormal />,
+    <BiBusSchool />,
+    <MdLocalGroceryStore />,
+  ];
+  return (
+    <div className="bg-white w-full px-32">
+      <ul className="flex w-full py-2 justify-between gap-4">
+        {icons.map((icon) => (
+          <li className="text-2xl ">{icon}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LinksHeader;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LinksHeader from "../components/LinksHeader";
+
+const Home = () => {
+  return (
+    <div>
+      <LinksHeader />
+    </div>
+  );
+};
+
+export default Home;
