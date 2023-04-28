571a5da8689f9093f1f5af1de1afc68509e2eb35
sam-c14 <samuelaffah14@gmail.com>
2023-04-28 17:46:00 +0100

    Deals section done

--- src/components/home/CurrentDeals.tsx
+++ src/components/home/CurrentDeals.tsx
@@ -11,19 +11,17 @@ const CurrentDeals = () => {
           <p className="text-sm">See All Items</p>
         </div>
       </div>
-      <div className="grid grid-cols-3 px-2 bg-white gap-2 py-2">
+      <div className="grid rounded-bl-md rounded-br-md grid-cols-3 px-2 bg-white gap-2 py-2">
         {deals.map((deal) => (
-          <div className="shadow-sm border bg-white items-center flex">
+          <div className="shadow-sm hover:shadow-lg transition-5 border bg-white items-center flex">
             <div className="w-1/4">
               <img className="w-full h-full" src={Deal1} alt="deal1" />
             </div>
             <div className="w-3/4">
               <p>iTec Handle Start, 3.5Kva Power...</p>
-              <div>
-                <p>
-                  <h5 className="font-bold text-lg">143,000</h5>
-                  <p className="line-through text-sm text-gray-500">207000</p>
-                </p>
+              <div className="flex gap-3 items-center mt-2">
+                <h5 className="font-bold text-lg">143,000</h5>
+                <p className="line-through text-sm text-gray-400">207000</p>
               </div>
               <p className="text-green-600 text-sm">You save #143,900</p>
             </div>
