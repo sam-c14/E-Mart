703a51f5b34351eb998055ae6ec1539fc6ba99d4
sam-c14 <samuelaffah14@gmail.com>
2023-04-28 14:03:37 +0100

    Slider Carousel Implemented

--- src/components/home/Slider.tsx
+++ src/components/home/Slider.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import SliderImg1 from "../../assets/images/SliderImg1.jpg";
+import SliderImg2 from "../../assets/images/SliderImg2.jpg";
+import SliderImg3 from "../../assets/images/SliderImg3.jpg";
 
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <div>
+      <Carousel showThumbs={false}>
+        <div>
+          <img src={SliderImg1} alt="img-1" />
+          {/* <p className="legend">Legend 1</p> */}
+        </div>
+        <div>
+          <img src={SliderImg2} alt="img-2" />
+          {/* <p className="legend">Legend 2</p> */}
+        </div>
+        <div>
+          <img src={SliderImg3} alt="img-3" />
+          {/* <p className="legend">Legend 3</p> */}
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Slider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import LinksHeader from "../components/home/nav/LinksHeader";
 import Navbar from "../components/home/nav/Navbar";
 import Categories from "../components/home/nav/Categories";
+import Slider from "../components/home/Slider";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <LinksHeader />
       <Navbar />
       <Categories />
+      <Slider />
     </div>
   );
 };
